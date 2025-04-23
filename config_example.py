51bb9b8311e48bd5d76c93c0623b7424c53b13aa
sourcyed <31018609+sourcyed@users.noreply.github.com>
2025-04-23 13:34:36 +0300

    Add example config file

---
+++ config_example.py
@@ -0,0 +1,6 @@
+from zoneinfo import ZoneInfo
+
+MOODLE_ICS_URL = "https://example.com/moodle.ics"
+GOOGLE_ICS_URL = "https://example.com/google.ics"
+DB_NAME = "events.db"
+LOCAL_TZ = ZoneInfo("Europe/Helsinki")
